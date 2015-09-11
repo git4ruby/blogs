aa7ce4061a9781dece38f9fec7575830737cb405
Mohit Chandana <chandanamohit@gmail.com>
2015-09-11 16:37:18 -0500

    Added publish option to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911061010) do
+ActiveRecord::Schema.define(version: 20150911211555) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -60,8 +60,9 @@ ActiveRecord::Schema.define(version: 20150911061010) do
     t.string   "name"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "publish",    default: false
   end
 
   create_table "users", force: :cascade do |t|
