7c2077f44cf7ed5e7e3ae34133c96986ba1393ff
Mohit Chandana <chandanamohit@gmail.com>
2015-09-11 16:03:03 -0500

    Added posts to activeadmin and fixed comments error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params.require(:comment).permit(:comment_text,:link))
+    @comment = @post.comments.create!(comment_params)
     redirect_to post_path(@post)
   end
 
@@ -10,6 +10,11 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+
+  def comment_params
+    params.require(:comment).permit(:commenter,:body)
   end
 end
 
